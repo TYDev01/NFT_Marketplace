6c54e6d1405660ce316e6276f0751b25421b878b
TYDev01 <anthonychinaemeru84@yahoo.com>
2025-05-21 13:00:23 +0100

    Animate the hero image

---
+++ src/components/Hero.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import style from '../Color.module.css';
+import ape1 from '../assets/arts/ape1.png';
+import ape2 from '../assets/arts/0.jpeg'; // Import your other images
+import ape3 from '../assets/arts/1.jpeg';
+import ape4 from '../assets/arts/2.jpeg';
+
+const Hero = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [ape1, ape2, ape3, ape4]; // Array of your images
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => 
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <div className='pt-25 pb-25 bg-[#010101]'>
+      <div className='flex items-center justify-around'>
+        <div className=''>
+          <h2 className={`${style.adFont} text-white`}>
+            Discover, <br /> Collect & Sell <br />Extraordinary <br /> NFTs
+          </h2>
+        </div>
+        <div className={style.imgContainer}>
+          <img 
+            src={images[currentImageIndex]} 
+            className={style.imgSize} 
+            alt="NFT Collection" 
+          />
+          <h2>Test</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from './Header'
+import Hero from './Hero'
 
 const Home = () => {
   return (
     <div>
-      <h2>Test</h2>
+        <Hero />
     </div>
   )
 }
